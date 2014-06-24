d44b91bc377c35de920e7a9e3e4604092b52ae94
Lenard Gunda <lenard@rebuildall.fi>
2014-06-24 13:27:22 +0300

    Added more 'real-life' like objects

---
+++ ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbUnitTestDemo
+{
+    public class ProductController
+    {
+        private readonly IProductRepository repo;
+        public ProductController ( IProductRepository repo )
+        {
+            this.repo = repo;
+        }
+        public Product ShowProduct( long id )
+        {
+            return repo.GetProducts().SingleOrDefault(p=>p.ProductId == id);
+        }
+    }
+}
---
+++ ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbUnitTestDemo
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+    }
+
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Entities entities;
+        public ProductRepository( Entities entities )
+        {
+            this.entities = entities;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return entities.Products;
+        }
+    }
+}
